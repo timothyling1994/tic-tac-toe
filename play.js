110efd19b09f7740443eb4f6b2d1d13977f7fbc0
Timothy Ling <timothyling1994@gmail.com>
2021-01-06 12:13:06 -0800

    Add player move functionality

--- play.js
+++ play.js
@@ -1,40 +1,62 @@
 let Gameboard = (() => {
-	
-	'use strict';
 
 	let Tile = {
 		mark: '',
 	};
 
-	let gameArr = new Array(9).fill(Tile);
+	let gameArr = new Array();
 
-	return {gameArr};
+	for(let i = 0;i<9;i++)
+	{
+		//clone Tile objects into Array
+		gameArr.push(JSON.parse(JSON.stringify(Tile)));
+	}
+
+	return {
+		gameArr,
+	};
 
 })();
 
 
 let GameController = (() => {
-	'use strict';
+
+	let isPlayerOneTurn = true;
 
 	let initGame = () => {
+
+		//grab player options (human vs ai)
 		DOMController.renderBoard();
 		
-		DOMController.getTiles();
-
 	};
 
+	let updategameArr = (tilePressed) => {
 
-	let validateMove = () => {
+		let index = tilePressed.getAttribute("id");
 
+		if(Gameboard.gameArr[index].mark == '')
+		{
+			if (isPlayerOneTurn)
+			{
+				Gameboard.gameArr[index].mark = "x";
+			}
+			else
+			{
+				Gameboard.gameArr[index].mark = "o";
+			}
+
+			isPlayerOneTurn = !isPlayerOneTurn;
+			DOMController.updateDOM(tilePressed); 
+		}
 	};
 
-	let updateBoard = (tile_num,player) => {
-		console.log(tile_num);
-		console.log(player);
+	let getPlayerTurn = () =>
+	{
+		return isPlayerOneTurn;
 	};
 
 	let clearBoard = () => {
-
+		
 	};
 
 	let signalWinner = () => {
@@ -43,7 +65,8 @@ let GameController = (() => {
 
 	return {
 		initGame,
-		updateBoard
+		updategameArr,
+		getPlayerTurn,
 	};
 })();
 
@@ -55,7 +78,7 @@ let DOMController = (() => {
 	{
 		let grid_container = document.querySelector("#grid-container");
 
-		for (let i=0;i<Gameboard.gameArr.length;i++)
+		for (let i=0; i<Gameboard.gameArr.length; i++)
 		{
 			let newDiv = document.createElement("div");
 			newDiv.classList.add('grid-box');
@@ -64,21 +87,39 @@ let DOMController = (() => {
 		}
 
 		tile_nodelist = document.querySelectorAll(".grid-box");
+
 		tile_nodelist.forEach(node => {
 			node.addEventListener("click", function(){
-				GameController.updateBoard(0,"player1"); 
+				GameController.updategameArr(this);
+				
 			},false);
 		});
+	};
+
+	let updateDOM = (tilePressed) => {
+		
+		let img = document.createElement('img');
+		console.log(tilePressed);
+		if (GameController.getPlayerTurn())
+		{
+			img.src="assets/o.png";
+			tilePressed.appendChild(img);
+		}
+		else
+		{
+			img.src="assets/x.png";
+			tilePressed.appendChild(img);
+		}
 	};
 
 	let getTiles = () => 
 	{
 		return tile_nodelist;
-	}
+	};
 
 	return {
 		
-		renderBoard,getTiles //tile_nodelist //getTiles
+		renderBoard,getTiles,updateDOM
 
 	};
 })();
